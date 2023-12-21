2db39f8677fa76115afa1d150d4652625ae0ecb3
victorzarzar <victorzarzar58@gmail.com>
2023-12-20 21:29:12 -0300

    finished task darkTheme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = ({ setTheme }) => {
           {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
         </div>
         <ul
-          className={`md:flex md:items-center md:pb-0 absolute md:static  md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-0 pl-9 bg-gray-800 sm:bg-transparent transition-all duration-500 ease-in ${open ? "top-12" : "top-[-490px]"}`}>
+          className={`md:flex md:items-center md:pb-0 absolute md:static  md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-0 pl-9 sm:bg-transparent transition-all duration-500 ease-in ${open ? "top-12" : "top-[-490px]"}`}>
           {Links.map((link) => (
             <li className="md:ml-8 md:my-0 my-3 font-semibold" key={link.name}>
               <Link
